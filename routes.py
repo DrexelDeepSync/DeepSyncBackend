9c4c41ffe0283a119253d28dbe6a4970ff20c8b5
vyoung708 <vyoung708@yahoo.com>
2022-02-28 21:32:24 -0500

    add ability to upload files

--- routes.py
+++ routes.py
@@ -1,10 +1,38 @@
 import enum
+
+from sympy import re
 from app import create_app
-from flask import request, jsonify
+from flask import request, send_file
+from werkzeug.utils import secure_filename
 import os
 import json
 
 app = create_app()
+
+def getFileNum():
+    return len([name for name in os.listdir('resources') if os.path.isfile(name)])
+
+@app.route("/uploadfile", methods=["POST"])
+def uploadFile():
+    rc = ReturnContent()
+
+    req_data = request.form
+
+    if not "fileName" in req_data:
+        rc.code = RequestCode.BadFileName
+        rc.message = "Missing file name"
+        return json.loads(rc.toJson())
+    
+    numFiles = getFileNum()
+
+    file_name = req_data['fileName'] + "_" + str(numFiles) + req_data['fileType']
+
+    f = request.files['file']
+    f.save(os.path.join("resources", secure_filename(file_name)))
+
+    rc.message = file_name
+
+    return json.loads(rc.toJson())
 
 @app.route("/fastaudio/generate", methods=["POST"])
 def generateFastAudio():
@@ -14,29 +42,33 @@ def generateFastAudio():
 
     req_json = request.get_json()
 
-    if not "audioPath" in req_json or not os.path.isfile(req_json["audioPath"]):
+    if not "audioPath" in req_json or not os.path.isfile(os.path.join("resources", req_json["audioPath"])):
         rc.code = RequestCode.BadAudio
         rc.message = "Missing audio file"
         return json.loads(rc.toJson())
     
     audio_path = req_json['audioPath']
 
-    if not "scriptPath" in req_json or not os.path.isfile(req_json["scriptPath"]):
+    if not "scriptPath" in req_json or not os.path.isfile(os.path.join("resources", req_json["scriptPath"])):
         rc.code = RequestCode.BadScript
         rc.message = "Missing script file"
         return json.loads(rc.toJson())
     
     script_path = req_json['scriptPath']
 
-    if not "outputPath" in req_json:
-        rc.code = RequestCode.BadOutput
-        rc.message = "Missing output path"
-        return json.loads(rc.toJson())
+    #if not "outputPath" in req_json:
+    #    rc.code = RequestCode.BadOutput
+    #    rc.message = "Missing output path"
+    #    return json.loads(rc.toJson())
 
-    output_path = req_json['outputPath']
+    outputFile = "audio" + str(getFileNum()) + ".wav"
+
+    output_path = os.path.join("resources", outputFile)
 
     fast_gen = FastAudioGenerator(audio_path, script_path, output_path)
     fast_gen.generateContent()
+
+    rc.message = outputFile
 
     return json.loads(rc.toJson())
 
@@ -48,31 +80,35 @@ def generateAudioVisual():
 
     rc = ReturnContent()
 
-    if not "audioPath" in req_json or not os.path.isfile(req_json["audioPath"]):
+    if not "audioPath" in req_json or not os.path.isfile(os.path.join("resources", req_json["audioPath"])):
         rc.code = RequestCode.BadAudio
         rc.message = "Missing audio file"
         return json.loads(rc.toJson())
 
-    audio_path = req_json['audioPath']
+    audio_path = os.path.join("resources", req_json["audioPath"])
 
-    if not "videoPath" in req_json or not os.path.isfile(req_json["videoPath"]):
+    if not "videoPath" in req_json or not os.path.isfile(os.path.join("resources", req_json["videoPath"])):
         rc.code = RequestCode.BadVideo
         rc.message = "Missing video file"
         return json.loads(rc.toJson())
 
-    video_path = req_json['videoPath']
+    video_path = os.path.join("resources", req_json["videoPath"])
 
-    if not "outputPath" in req_json:
-        rc.code = RequestCode.BadOutput
-        rc.message = "Missing output path"
-        return json.loads(rc.toJson())
+    #if not "outputPath" in req_json:
+    #    rc.code = RequestCode.BadOutput
+    #    rc.message = "Missing output path"
+    #    return json.loads(rc.toJson())
 
-    output_path = req_json['outputPath']
+    outputFile = "fakedvideo_" + str(getFileNum()) + ".mp4"
+
+    output_path = os.path.join("resources", outputFile)
 
     video_gen = AudioVisualGenerator(audio_path, video_path, output_path)
     video_gen.generateContent()
 
-    return json.loads(rc.toJson())
+    rc.message = output_path
+
+    return send_file(output_path, mimetype = 'video/mp4')
 
 class ReturnContent():
     def __init__(self):
@@ -82,13 +118,13 @@ class ReturnContent():
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
 
-
 class RequestCode(str, enum.Enum):
     Success: str = "200"
     BadAudio: str = "401"
     BadVideo: str = "402"
     BadScript: str = "403"
     BadOutput: str = "405"
+    BadFileName: str = "406"
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
