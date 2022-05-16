b001ed8cc998e7212cf8a1d68f6904f4f38331d2
vyoung708 <vyoung708@yahoo.com>
2022-05-15 20:28:25 -0400

    change video res

--- src/generators/AudioVisualGenerator.py
+++ src/generators/AudioVisualGenerator.py
@@ -1,7 +1,6 @@
 from src.generators.ContentGenerator import ContentGenerator
 from libs.Wav2Lip.create_lip_sync import LipSyncer
 import cv2
-import numpy as np
 
 class AudioVisualGenerator(ContentGenerator):
     def __init__(self, audioPath: str, videoPath: str, outputPath: str):
@@ -9,11 +8,38 @@ class AudioVisualGenerator(ContentGenerator):
         self._videoPath = videoPath
         self._outputPath = outputPath
 
-    def generateContent(self) -> None:
+    def rescale_frame(self, frame_input, percent=.75):
+        width = int(frame_input.shape[1] * percent)
+        height = int(frame_input.shape[0] * percent)
+        dim = (width, height)
+        return cv2.resize(frame_input, dim, interpolation=cv2.INTER_AREA)
+
+    def resize_video(self):
         vid = cv2.VideoCapture(self._videoPath)
         height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        resize = 1
+        percent = 720/height
         if height > 720:
-            resize = int(np.ceil(height/720))
+            if vid.isOpened():
+                _, frame = vid.read()
+                rescaled_frame = self.rescale_frame(frame, percent)
+                (h, w) = rescaled_frame.shape[:2]
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                writer = cv2.VideoWriter('Video_output.mp4',
+                                        fourcc, 15.0,
+                                        (w, h), True)
+            
+            while vid.isOpened():
+                _, frame = vid.read()
+
+                rescaled_frame = self.rescale_frame(frame, percent)
+
+                # write the output frame to file
+                writer.write(rescaled_frame)
+            
+            vid.release()
+            writer.release()
+
+    def generateContent(self) -> None:
+        self.resize_video()
         ls = LipSyncer()
-        ls.start_generation(self._videoPath, self._audioPath, self._outputPath, resize)
+        ls.start_generation(self._videoPath, self._audioPath, self._outputPath)
